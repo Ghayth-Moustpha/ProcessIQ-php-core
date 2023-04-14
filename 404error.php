0f40c160764838a79fdd2db86a83424b14669be3
Ghayth Moustpha <53100059+Ghayth-Moustpha@users.noreply.github.com>
2023-04-14 15:52:11 +0300

    First Commit

---
+++ 404error.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> RMI Login </title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="./indexStyle/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./indexStyle/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./indexStyle/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="./indexStyle/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./indexStyle/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./indexStyle/css/util.css">
+	<link rel="stylesheet" type="text/css" href="./indexStyle/css/main.css">
+
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+			
+
+				<div id="main">
+    	<div class="fof">
+        		<h1>Error 404</h1>
+    	</div>
+</div>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="./indexStyle/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="./indexStyle/vendor/bootstrap/js/popper.js"></script>
+	<script src="./indexStyle/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="./indexStyle/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="./indexStyle/vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="./indexStyle/js/main.js"></script>
+
+</body>
+</html>
